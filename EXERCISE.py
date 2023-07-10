5dcb1f04f9184b08cb0f3e6773e9c35c42f04796
fatimanaqueeb <137386747+fatimanaqueeb@users.noreply.github.com>
2023-07-10 14:52:06 +0530

    Add files via upload

---
+++ EXERCISE.py
@@ -0,0 +1,15 @@
+n = input("enter number 1: ") 
+y = input("enter number 2: ")
+ans1 =n+y
+print ("addition of" ,n, "and" ,y, "is" ,ans1)
+ans2 =n-y
+print("subtraction of" ,n, "and" ,y, "is" ,ans2)
+ans3 =n/y
+print("division of" ,n, "and" ,y, "is" ,ans3)
+ans4 =n*y
+print ("multiplication of" ,n, "and" ,y, "is" ,ans4)
+ans5 =n%y
+print ("modulus of" ,n, "and" ,y,"is" ,ans5)
+ans6 =n//y
+print ("floor division of" ,n, "and" ,y, "is" ,ans6)
+
